9a297ffc08d8e872793003f3dc0442407eb9c727
WiesenWasch <a.c.meadows@wustl.edu>
2025-12-04 10:06:31 +0100

    add model inference logging

--- api/app.py
+++ api/app.py
@@ -1,6 +1,8 @@
+import time
 from fastapi import FastAPI, Response
 from pydantic import BaseModel
 import mlflow
+import numpy as np
 
 class Features(BaseModel):
     data: list[dict]
@@ -15,6 +17,7 @@ class MyModel:
 MODEL = MyModel()
 
 app = FastAPI(title="CancerClassificationAPI")
+mlflow.set_experiment("mlflow_monitoring")
 
 @app.get("/health")
 def health():
@@ -31,5 +34,13 @@ def predict(features: Features, _response: Response):
         MODEL.model = mlflow.pyfunc.load_model(f"models:/{model_id}")
 
     # df = pd.DataFrame(features.data) # features need to be ordered but this is an OPs not dev project
+    start = time.time()
     preds = MODEL.model.predict(features.data)
-    return {"predictions": preds.tolist()}
+    latency = time.time() - start
+
+    # logging model latency in code
+    with mlflow.start_run(run_name="inference", nested=True):
+        mlflow.log_metric("latency_ms", latency * 1000)
+        mlflow.log_param("batch_size", len(features.data))
+
+    return {"predictions": preds.tolist(), "prediciton_latency":latency}
